781e1bad096c5c5a7eb946a2d389dbb6b580ba46
Andrey Selin <andreiprostoandrei@gmail.com>
2020-11-24 20:26:18 +0100

    perform testing

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
      host:     '54.244.173.135',
-     user:     '',
-     password: '',
-     database: '',
+     user:     'ttoko',
+     password: 'ttoko',
+     database: 'ttoko',
      connectionLimit: 5
 });
 
@@ -18,7 +18,7 @@ const query = async (query, arguments) => {
     try {
         const connection = await pool.getConnection();
         try {
-            await connection.query(query, arguments);
+            return await connection.query(query, arguments);
         } catch (e) {
             console.error('DB error single: ', query, arguments, e);
         }
@@ -57,7 +57,7 @@ const someBusinessLogicMethod = async () => {
     // Each query uses its own pool connection
     // More laconic way to make single query
 
-    const insertionResult = await maria.query('INSERT INTO tmpTable (name) VALUES ("?")', ['tmpValue']);
+    const insertionResult = await maria.query('INSERT INTO tmpTable (name) VALUES (?)', ['tmpValue']);
     console.log('=> 1', insertionResult);
 
     const selectionResult = await maria.query('SELECT * FROM tmpTable');
@@ -69,7 +69,7 @@ const someBusinessLogicMethod = async () => {
 
     await maria.flow(async (query) => {
 
-        const updateInFlowResult = await query('UPDATE tmpTable SET name="?" WHERE id = ?', ['newName', 2]);
+        const updateInFlowResult = await query('UPDATE tmpTable SET name=? WHERE id = ?', ['anotherName', 3]);
         console.log('=> 3', updateInFlowResult);
 
         const selectInFlowResult = await query('SELECT name FROM tmpTable WHERE id = ?', [3]);
